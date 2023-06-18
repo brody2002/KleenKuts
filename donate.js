38e2d49c4845363ca0d6450dd7874231abc0306d
Brody Roberts <72287521+brody2002@users.noreply.github.com>
2023-06-18 14:01:06 -0700

    The Final Version of the website from Hack Davis.
    I intend to make this an actual website for general use for my friend. So Focus will begin shifting away from the homeless people haircuts.

---
+++ donate.js
@@ -0,0 +1,9 @@
+function openPopup() {
+  document.getElementById("popupDonate").style.display = "block";
+  document.getElementById("confettis").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popupDonate").style.display = "none";
+  document.getElementById("confettis").style.display = "none";
+}
